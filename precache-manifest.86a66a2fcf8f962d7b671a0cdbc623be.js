d87b862ecc670fd4ffd2e6f1060b95938ea2e65c
Code Sandbox <hello@codesandbox.io>
2021-06-14 15:57:26 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.86a66a2fcf8f962d7b671a0cdbc623be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b4d95dd5733c8ecb8c613ad4324841",
+    "url": "https://charles-d7625.github.io/csb-64g13/index.html"
+  },
+  {
+    "revision": "7e486152120ccc32410e",
+    "url": "https://charles-d7625.github.io/csb-64g13/static/css/main.8f70fc69.chunk.css"
+  },
+  {
+    "revision": "ec12751b7ec51a088105",
+    "url": "https://charles-d7625.github.io/csb-64g13/static/js/2.a9205a14.chunk.js"
+  },
+  {
+    "revision": "eadc8ac8b6080df510389a008a76ce0a",
+    "url": "https://charles-d7625.github.io/csb-64g13/static/js/2.a9205a14.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e486152120ccc32410e",
+    "url": "https://charles-d7625.github.io/csb-64g13/static/js/main.d2a94fc3.chunk.js"
+  },
+  {
+    "revision": "f4952a572cfea0f97977",
+    "url": "https://charles-d7625.github.io/csb-64g13/static/js/runtime-main.834fc0bc.js"
+  }
+]);
